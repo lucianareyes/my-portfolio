be09c26ad2fd971fa0a45416f8893bb6f41d7414
lucianareyes <lucianareyes@gmail.com>
2020-07-31 11:24:36 +0200

    Fix image grid 2

--- src/ui/Image.js
+++ src/ui/Image.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Image({ src, alt, className, width, height }) {
   return (
-    <picture>
-      <source srcset={`${src}.webp`} type="image/webp" />
-      <source srcset={`${src}.png`} type="image/png" />
+    <picture width={width} height={height}>
+      <source srcSet={`${src}.webp`} type="image/webp" />
+      <source srcSet={`${src}.png`} type="image/png" />
       <img
         src={`${src}.png`}
         alt={alt}
