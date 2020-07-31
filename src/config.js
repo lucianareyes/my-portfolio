1f90a7998c1a86e8f2fd84e74c0859f104c2ae2b
lucianareyes <lucianareyes@gmail.com>
2020-07-31 16:09:22 +0200

    Fix small issues

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-  imgBaseURL: "https://s3.eu-central-1.amazonaws.com/img.lucianareyes.com",
+  imgBaseURL:
+    process.env.NODE_ENV === "development"
+      ? `${process.env.PUBLIC_URL}/img`
+      : "https://s3.eu-central-1.amazonaws.com/img.lucianareyes.com",
 };
